5d589e39ccd435ea890ca5488371c9fa092a65d9
Biswarup <chatterjeebiswarup61@gmail.com>
2025-10-27 23:12:51 +0530

    hero section complete

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -5,41 +5,6 @@ export default function Home() {
         <>
             <Hero />
 
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui perferendis atque velit minima reiciendis facilis, tempora expedita sit blanditiis aliquid quo ipsum, repellendus consectetur unde neque nostrum debitis minus quia.
-            Molestiae quis at minus ducimus quibusdam aut unde architecto maxime, illo ex, praesentium labore, tempora quisquam ipsam distinctio nisi eligendi animi. Ipsa nesciunt mollitia quibusdam numquam fugit, illo quos exercitationem.
-            Corporis sint illo adipisci deserunt, dolores aut at non libero perferendis a cum, ipsum facere labore deleniti explicabo sit consequuntur repudiandae? Molestiae quos ut nesciunt, voluptate corporis enim vitae aliquam.
-            Dolorum magnam voluptatum ipsam dolorem consequatur corrupti, quisquam perspiciatis esse nesciunt excepturi facilis repudiandae in provident eius, facere, fuga eaque cum. Quas voluptatibus est quo? Minus corporis sit commodi fugit.
-            Iure exercitationem, odit rem fugit fuga numquam a non dolorem sint nostrum fugiat quaerat pariatur corporis debitis, eaque voluptatum quia tenetur? Quaerat unde voluptatibus quam repudiandae deleniti, consequuntur ut eaque.
-            Architecto laborum minima cumque. Eveniet corrupti totam, aut, numquam itaque qui iusto aperiam veniam nulla ducimus ut excepturi illum sapiente tenetur dignissimos soluta, consequuntur dolore possimus unde harum deserunt ipsam?
-            Quod quae veniam officiis molestias porro accusantium quasi, sapiente fuga, odio dicta amet veritatis voluptatum dolorum accusamus ratione aliquid vitae. Deserunt laboriosam atque animi tempore. Nulla non quaerat sint. Maiores.
-            Vitae eaque accusamus cumque repellendus amet neque. Eveniet ratione ipsam perspiciatis iste molestiae nihil tempore iusto voluptate eius eum mollitia harum ab veritatis fugiat aperiam doloremque cum officia, ex ipsa!
-            Fugit, amet. Quam nobis atque sapiente itaque sunt provident, commodi reiciendis fugiat expedita iure officiis impedit esse deserunt! Quam corrupti minus consequuntur labore maiores mollitia laudantium numquam odit sequi aperiam.
-            Ut obcaecati sint dolor optio perspiciatis laborum qui iusto rem sequi laudantium quia cumque at veritatis consequuntur, voluptas facilis tempore illo nulla? Aliquam ipsa dicta ducimus numquam aliquid neque nulla.
-            Quia repellendus quibusdam nostrum sed aliquid illo! Consequatur maiores repudiandae quos eum provident magni dolores, velit illum, laboriosam natus cumque quaerat corporis deserunt cupiditate nisi ea tempore vero? Corporis, voluptas!
-            Adipisci dolores explicabo esse voluptas magni assumenda? Error veniam rem iste id laborum dolorem vitae reiciendis, nemo, excepturi aperiam quis saepe molestiae, deleniti molestias aliquid? Nam unde soluta sed consequuntur?
-            Eum sequi optio impedit perferendis laudantium quos cum accusantium! Optio earum rerum recusandae perferendis quasi! Impedit magni exercitationem quisquam nihil, deleniti facilis iure beatae, qui quia molestiae fuga aperiam eveniet.
-            Ab veritatis soluta perspiciatis temporibus atque. At fugit obcaecati enim, nam corporis unde magni minima dolore sequi perspiciatis harum mollitia sit dolorem tempora minus reiciendis, ea vel facere praesentium veniam.
-            Dolorum magnam voluptatum ipsam dolorem consequatur corrupti, quisquam perspiciatis esse nesciunt excepturi facilis repudiandae in provident eius, facere, fuga eaque cum. Quas voluptatibus est quo? Minus corporis sit commodi fugit.
-            Iure exercitationem, odit rem fugit fuga numquam a non dolorem sint nostrum fugiat quaerat pariatur corporis debitis, eaque voluptatum quia tenetur? Quaerat unde voluptatibus quam repudiandae deleniti, consequuntur ut eaque.
-            Architecto laborum minima cumque. Eveniet corrupti totam, aut, numquam itaque qui iusto aperiam veniam nulla ducimus ut excepturi illum sapiente tenetur dignissimos soluta, consequuntur dolore possimus unde harum deserunt ipsam?
-            Quod quae veniam officiis molestias porro accusantium quasi, sapiente fuga, odio dicta amet veritatis voluptatum dolorum accusamus ratione aliquid vitae. Deserunt laboriosam atque animi tempore. Nulla non quaerat sint. Maiores.
-            Vitae eaque accusamus cumque repellendus amet neque. Eveniet ratione ipsam perspiciatis iste molestiae nihil tempore iusto voluptate eius eum mollitia harum ab veritatis fugiat aperiam doloremque cum officia, ex ipsa!
-            Fugit, amet. Quam nobis atque sapiente itaque sunt provident, commodi reiciendis fugiat expedita iure officiis impedit esse deserunt! Quam corrupti minus consequuntur labore maiores mollitia laudantium numquam odit sequi aperiam.
-            Ut obcaecati sint dolor optio perspiciatis laborum qui iusto rem sequi laudantium quia cumque at veritatis consequuntur, voluptas facilis tempore illo nulla? Aliquam ipsa dicta ducimus numquam aliquid neque nulla.
-            Quia repellendus quibusdam nostrum sed aliquid illo! Consequatur maiores repudiandae quos eum provident magni dolores, velit illum, laboriosam natus cumque quaerat corporis deserunt cupiditate nisi ea tempore vero? Corporis, voluptas!
-            Adipisci dolores explicabo esse voluptas magni assumenda? Error veniam rem iste id laborum dolorem vitae reiciendis, nemo, excepturi aperiam quis saepe molestiae, deleniti molestias aliquid? Nam unde soluta sed consequuntur?
-            Eum sequi optio impedit perferendis laudantium quos cum accusantium! Optio earum rerum recusandae perferendis quasi! Impedit magni exercitationem quisquam nihil, deleniti facilis iure beatae, qui quia molestiae fuga aperiam eveniet.
-            Ab veritatis soluta perspiciatis temporibus atque. At fugit obcaecati enim, nam corporis unde magni minima dolore sequi perspiciatis harum mollitia sit dolorem tempora minus reiciendis, ea vel facere praesentium veniam.
-            Quibusdam porro quae labore, dolore amet repellat itaque unde eum in repudiandae, consequuntur sunt, tenetur velit veniam! Illum ipsa, autem harum ipsum dicta laudantium voluptas voluptatem. Ullam accusamus quae facere!
-            Sit dolorum voluptates odio eaque deleniti? Consectetur dolorem recusandae iusto officiis qui molestiae dicta inventore at deserunt velit? Maiores amet tempore similique ea consequuntur qui a accusamus culpa sint eveniet.
-            Ab perspiciatis harum facere ratione cupiditate, inventore iste. Aspernatur provident, dignissimos quibusdam inventore ipsa fugiat fuga adipisci quia, architecto sapiente amet? Atque ducimus iure obcaecati, praesentium nostrum id sint dicta.
-            Nobis animi quidem rem fugiat a voluptas corporis autem mollitia sint minima sequi aperiam vel similique sapiente veritatis voluptatibus, fugit eos, quod cum at veniam illo ex! Magni, quas alias.
-            Obcaecati repellat quae non veniam enim rerum doloremque. Velit accusamus beatae maiores impedit nulla aperiam repellat doloremque. Voluptatum numquam accusantium praesentium neque provident nesciunt cumque placeat. Non porro velit minus.
-            Debitis dolor consequatur ipsa explicabo dolore qui nemo labore autem expedita nam! Excepturi, vero. Vero quod rerum alias mollitia quis dicta porro libero unde dolores dolorem incidunt, qui explicabo atque.
-            Modi quos aperiam iste ipsam sit, repellat laborum natus laudantium cupiditate eos quia! Quia libero incidunt officiis corrupti labore, id laboriosam delectus! Molestiae provident a earum? At sequi explicabo deleniti.
-            Explicabo veniam repellat distinctio atque neque accusamus, tempora commodi praesentium, harum accusantium iste error laborum ex reiciendis nobis magni, sed odio itaque dolor nesciunt ipsum tenetur deserunt possimus qui. Dolorem?
-            Laudantium qui voluptas eligendi! Quasi, rerum dolores maiores nihil officiis laudantium debitis, molestias voluptatibus unde recusandae, sequi aperiam. Sit molestiae iusto itaque dolor esse, cupiditate amet facere exercitationem beatae libero.
-
         </>
     );
 }
--- src/pages/home/components/Hero.jsx
+++ src/pages/home/components/Hero.jsx
@@ -1,36 +1,220 @@
-// Hero.jsx
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import {
+    Box,
+    Typography,
+    TextField,
+    Button,
+    InputAdornment,
+    Grid,
+} from "@mui/material";
+import { Search } from "@mui/icons-material";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/autoplay";
+import theme from "../../../theme/theme";
+
+const reviews = [
+    {
+        name: "M",
+        text: "Just got back from a week-long, 20–26th January, all girls tour package of Manali–Kasol–Jibhi.",
+    },
+    {
+        name: "G",
+        text: "December 2023 New Year – I’ve recently done Jaisalmer weekend trip, accommodation in Jaisalmer was perfect!",
+    },
+    {
+        name: "A",
+        text: "The Kashmir Great Lake Trek with JustWravel was an absolute dream come true. Words can’t fully capture it!",
+    },
+    {
+        name: "R",
+        text: "An unforgettable Meghalaya trip — amazing waterfalls, homestays, and super friendly group!",
+    },
+    {
+        name: "S",
+        text: "Our Spiti Valley road trip was beyond expectations. The team managed everything flawlessly!",
+    },
+    {
+        name: "T",
+        text: "Leh-Ladakh bike expedition was thrilling! Excellent guides and great company all through.",
+    },
+];
+
+const locations = ["Meghalaya", "Goa", "Manali", "Ladakh", "Kerala"];
+
+const height = "100vh"
 
 const Hero = () => {
     return (
-        <Box
-            component="section"
-            sx={{
-                height: "70vh",
-                width: "100%",
-                backgroundImage: `linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)), url("https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2070")`,
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-                position: "absolute",
-                top: 0,
-                left: 0,
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                color: "#fff",
-                textAlign: "center",
-                px: 2, // padding on sides for small screens
-            }}
-        >
-            <Typography variant="h2" sx={{ fontWeight: "bold", mb: 2 }}>
-                Welcome to My Website
-            </Typography>
-            <Typography variant="h5">
-                Crafting amazing experiences from code to design
-            </Typography>
-        </Box>
+        <>
+            <Box
+                sx={{
+                    position: "absolute",
+                    top: 0,
+                    opacity: 0.9,
+                    height: height,
+                    width: "100%",
+                    backgroundImage: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
+                url("https://images.unsplash.com/photo-1476514525535-07fb3b4ae5f1?ixlib=rb-4.1.0&auto=format&fit=crop&q=80&w=2070")`,
+                    backgroundSize: "cover",
+                    backgroundPosition: "center",
+                    color: "#fff",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    px: { xs: 3, md: 10 },
+                    overflow: "hidden",
+                }}
+            >
+                <Grid container spacing={4} alignItems="center">
+                    {/* Left Text Content */}
+                    <Grid size={{ xs: 12, md: 7 }}>
+                        <Typography
+                            variant="h2"
+                            sx={{
+                                fontWeight: "bold",
+                                lineHeight: 1.2,
+                                fontSize: { xs: "2.5rem", md: "4rem" },
+                            }}
+                        >
+                            Book Your <br /> Trip to{" "}
+                            <Box component="span" sx={{ color: theme.palette.secondary.main }}>
+                                Meghalaya
+                            </Box>
+                        </Typography>
+                    </Grid>
+
+                    {/* Right Side - Swiper Reviews */}
+                    <Grid
+                        size={{ xs: 12, md: 5 }}
+                        sx={{
+                            display: "flex",
+                            justifyContent: "flex-end",
+                            mt: { xs: 4, md: 0 },
+                        }}
+                    >
+                        <Box
+                            sx={{
+                                width: "100%",
+                                maxWidth: 400,
+                            }}
+                        >
+                            <Swiper
+                                direction="vertical"
+                                slidesPerView={2}
+                                spaceBetween={20}
+                                autoplay={{ delay: 2500, disableOnInteraction: false }}
+                                loop
+                                modules={[Autoplay]}
+                                style={{ height: "350px" }}
+                            >
+                                {reviews.map((review, index) => (
+                                    <SwiperSlide key={index}>
+                                        <Box
+                                            sx={{
+                                                background: "rgba(255, 255, 255, 0.08)",
+                                                borderRadius: "16px",
+                                                p: 3,
+                                                color: "#fff",
+                                                backdropFilter: "blur(8px)",
+                                                boxShadow: "0 4px 20px rgba(0,0,0,0.2)",
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                gap: 1.5,
+                                                transition: "all 0.3s ease",
+                                                "&:hover": {
+                                                    transform: "translateY(-4px)",
+                                                    background: "rgba(255,255,255,0.12)",
+                                                },
+                                            }}
+                                        >
+                                            {/* Header (Avatar + Stars) */}
+                                            <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
+                                                <Box
+                                                    sx={{
+                                                        width: 40,
+                                                        height: 40,
+                                                        borderRadius: "50%",
+                                                        background: "linear-gradient(135deg, #00B4DB, #0083B0)",
+                                                        display: "flex",
+                                                        justifyContent: "center",
+                                                        alignItems: "center",
+                                                        fontWeight: "bold",
+                                                        fontSize: "1.1rem",
+                                                    }}
+                                                >
+                                                    {review.name}
+                                                </Box>
+                                                <Typography sx={{ color: "#FFD700", fontSize: "1rem" }}>★★★★★</Typography>
+                                            </Box>
+
+                                            {/* Review Text */}
+                                            <Typography
+                                                variant="body2"
+                                                sx={{
+                                                    lineHeight: 1.5,
+                                                    fontSize: "0.95rem",
+                                                    opacity: 0.95,
+                                                }}
+                                            >
+                                                {review.text}
+                                            </Typography>
+                                        </Box>
+
+                                    </SwiperSlide>
+                                ))}
+                            </Swiper>
+                        </Box>
+                    </Grid>
+                </Grid>
+
+                {/* Stats Bar (Bottom Fixed Inside Hero) */}
+                <Box
+                    sx={{
+                        position: "absolute",
+                        bottom: 0,
+                        left: 0,
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "space-around",
+                        alignItems: "center",
+                        py: 3,
+                        background:
+                            "linear-gradient(0deg, rgba(0, 0, 0, 0.6) 0%, rgba(0,0,0,0) 100%)",
+                        // backdropFilter: "blur(1px)",
+                        flexWrap: "wrap",
+                    }}
+                >
+                    {[
+                        { number: "10000+", label: "Reviews" },
+                        { number: "80000+", label: "Satisfied Travelers" },
+                        { number: "50+", label: "Destinations" },
+                        { number: "8 Years", label: "Experience" },
+                    ].map((item, index) => (
+                        <Box
+                            key={index}
+                            sx={{
+                                textAlign: "center",
+                                minWidth: { xs: "40%", sm: "20%" },
+                                mb: { xs: 2, sm: 0 },
+                                color: "#fff",
+                            }}
+                        >
+                            <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+                                {item.number}
+                            </Typography>
+                            <Typography variant="body2" sx={{ opacity: 0.9 }}>
+                                {item.label}
+                            </Typography>
+                        </Box>
+                    ))}
+                </Box>
+            </Box>
+            <Box sx={{ height: height }}>
+
+            </Box>
+        </>
     );
 };
 
